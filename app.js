778f0307b1022a69ee46f6901d28d5ac0ff04598
rishabh506 <rishabhgupta3451@gmail.com>
2026-07-24 13:05:17 +0530

    Fix test script

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Jenkins CI/CD Pipeline!\n');
+  res.end('Hello from Jenkins CI/CD Pipeline! Rishabh This side\n');
 });
 
 server.listen(3000, () => console.log('App running on port 3000'));
